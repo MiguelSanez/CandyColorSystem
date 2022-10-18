7d02f93f233da37d8d15564e75c6f84e682e0890
arckenos <73735400+arckenos@users.noreply.github.com>
2022-10-17 21:53:19 -0700

    arreglado repos
    todo bien

--- src/repositories/index.js
+++ src/repositories/index.js
@@ -1,5 +1,16 @@
 const UsersRepository = require('./users.repository');
+const DetallesRepository = require('./detalle.repository');
+const ProveedoresRepository = require('./proveedor.repository');
+const ClientesRepository = require('./clientes.repository');
+const ProductosRepository = require('./producto.repository');
+const DocumentosRepository = require('./documento.repository');
 
 module.exports = {
-    UsersRepository
+    UsersRepository,
+    DetallesRepository,
+    ProveedoresRepository,
+    ClientesRepository,
+    ProductosRepository,
+    DocumentosRepository
+
 };
