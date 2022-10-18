93a22e65864ceec00be8d9ec0da6fe509d438d66
arckenos <73735400+arckenos@users.noreply.github.com>
2022-10-17 21:47:07 -0700

    nuevos repositorios
    añadidos:
    clientes
    detalle
    proveedor

---
+++ src/repositories/detalle.repository.js
@@ -0,0 +1,37 @@
+const db = require('./../models');
+
+class DetallesRepository {
+
+    async find() {
+        const detalle = await db.detalle.findAll();
+        return detalle;
+    }
+
+    async findUserById(id) {
+        const detalle = await db.detalle.findOne({
+            where: { id },
+        });
+
+        return detalle;
+    }
+
+    async add(detalle) {
+        detalle.id = 0;
+        return (await detalle.save());
+    }
+
+    async update(detalle) {
+        if (detalle.id <= 0)
+            throw new Error('Expecting user to have a valid id.');
+
+        return (await detalle.save());
+    }
+
+    async delete(detalle) {
+        return (await detalle.destroy());
+    }
+
+
+};
+
+module.exports = DetallesRepository;
---
+++ src/repositories/proveedor.js
@@ -0,0 +1,37 @@
+const db = require('./../models');
+
+class ProveedoresRepository {
+
+    async find() {
+        const proveedor = await db.proveedor.findAll();
+        return proveedor;
+    }
+
+    async findUserById(id) {
+        const proveedor = await db.proveedor.findOne({
+            where: { id },
+        });
+
+        return proveedor;
+    }
+
+    async add(proveedor) {
+        proveedor.id = 0;
+        return (await proveedor.save());
+    }
+
+    async update(proveedor) {
+        if (proveedor.id <= 0)
+            throw new Error('Expecting user to have a valid id.');
+
+        return (await proveedor.save());
+    }
+
+    async delete(proveedor) {
+        return (await proveedor.destroy());
+    }
+
+
+};
+
+module.exports = ProveedoresRepository;
